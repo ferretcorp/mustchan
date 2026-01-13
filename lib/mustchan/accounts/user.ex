466d76a6c9aa8ff06f288efc2364c5fca46148c3
Matei <matei3d@gmail.com>
2026-01-12 21:32:43 -0500

    Security: upgraded to Argon2 for zased hashing.

--- lib/mustchan/accounts/user.ex
+++ lib/mustchan/accounts/user.ex
@@ -95,11 +95,11 @@ defmodule Mustchan.Accounts.User do
 
     if hash_password? && password && changeset.valid? do
       changeset
-      # If using Bcrypt, then further validate it is at most 72 bytes long
-      |> validate_length(:password, max: 72, count: :bytes)
+      # We're using Argon2, so the password can be as long as it wants, but we'll validate it is at most 256 bytes long
+      |> validate_length(:password, max: 256, count: :bytes)
       # Hashing could be done with `Ecto.Changeset.prepare_changes/2`, but that
       # would keep the database transaction open longer and hurt performance.
-      |> put_change(:hashed_password, Bcrypt.hash_pwd_salt(password))
+      |> put_change(:hashed_password, Argon2.hash_pwd_salt(password))
       |> delete_change(:password)
     else
       changeset
@@ -118,15 +118,15 @@ defmodule Mustchan.Accounts.User do
   Verifies the password.
 
   If there is no user or the user doesn't have a password, we call
-  `Bcrypt.no_user_verify/0` to avoid timing attacks.
+  `Argon2.no_user_verify/0` to avoid timing attacks.
   """
   def valid_password?(%Mustchan.Accounts.User{hashed_password: hashed_password}, password)
       when is_binary(hashed_password) and byte_size(password) > 0 do
-    Bcrypt.verify_pass(password, hashed_password)
+    Argon2.verify_pass(password, hashed_password)
   end
 
   def valid_password?(_, _) do
-    Bcrypt.no_user_verify()
+    Argon2.no_user_verify()
     false
   end
 end
